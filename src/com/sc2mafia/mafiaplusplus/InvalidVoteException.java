d2dee305632fb9f1cef420b43dc414d80f262f47
oops_ur_dead <oops_ur_dead@gmail.com>
2013-09-03 17:40:38 -0400

    Made constructor not public

--- src/com/sc2mafia/mafiaplusplus/InvalidVoteException.java
+++ src/com/sc2mafia/mafiaplusplus/InvalidVoteException.java
@@ -4,7 +4,7 @@ public class InvalidVoteException extends Exception {
 
     private static final long serialVersionUID = 3745964923257331587L;
 
-    public InvalidVoteException (String message) {
+    InvalidVoteException (String message) {
 	super(message);
     }
 }
